e05025aec9e25e867fc456f2688a08ebecf77112
Salma Cruz Sánchez <salmasanchezscs@outlook.com>
2021-06-16 20:48:17 -0500

    Historico.php
    Se agrega la referencia a la vista del reporte tienda.

--- historico.php
+++ historico.php
@@ -42,7 +42,7 @@
                             <li class="nav-item"> <a class="dropdown-item " href="#"> 
                                 <i ></i><span> Tiempo </span> </a>
                             </li>
-                            <li class="nav-item"> <a class="dropdown-item " href="#"> 
+                            <li class="nav-item"> <a class="dropdown-item " href="reporte_tienda.php"> 
                                <i ></i><span> Tiendas </span> </a>
                             </li>
                         </ul>
@@ -73,21 +73,6 @@
                     <h3 class="text-dark mb-4">Histórico</h3>
                     <div class="card shadow">
                         <div class="card-body">
-                            <!--<div class="row">
-                                <div class="col-md-6 text-nowrap">
-                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
-                                        <label class="form-label">Show&nbsp;
-                                            <select class="d-inline-block form-select form-select-sm">
-                                                <option value="10" selected="">10</option>
-                                                <option value="25">25</option>
-                                                <option value="50">50</option>
-                                                <option value="100">100</option>
-                                                <option value="100">Todo</option>
-                                            </select>&nbsp;
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>-->
                             <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
                                 <table class="table my-0" id="dataTable">
                                     <thead>
@@ -122,29 +107,13 @@
                                     </tfoot>
                                 </table>
                             </div>
-                            <!--<div class="row">
-                                <div class="col-md-6 align-self-center">
-                                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
-                                </div>
-                                <div class="col-md-6">
-                                    <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
-                                        <ul class="pagination">
-                                            <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-                                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                            </div> -->
                         </div>
                     </div>
                 </div>
             </div>
             <footer class="bg-white sticky-footer">
                 <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © Brand 2021</span></div>
+                    <div class="text-center my-auto copyright"><span>4CV70 - EQUIPO 5</span></div>
                 </div>
             </footer>
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
